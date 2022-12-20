526ac2333938a2d3fc8c5180bf9209267fbcc018
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-20 12:57:26 +0600

    implemented stack

---
+++ Stack.js
@@ -0,0 +1,14 @@
+let numbers = [];
+
+numbers.push(1);
+numbers.push(5);
+numbers.push(8);
+numbers.push(4);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
